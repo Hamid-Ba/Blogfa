fcf0ae247a9526fc060aa3962342a43fb8dfe236
hamidba <balalzadehhamid79@gmail.com>
2022-04-14 23:57:11 +0430

    Complete Role Domain Service Implementation

---
+++ Blog/Blogfa.Application/RoleAgg/RoleDomainService.cs
@@ -0,0 +1,15 @@
+﻿using Blogfa.Domain.RoleAgg.Repository;
+using Blogfa.Domain.RoleAgg.Services;
+
+namespace Blogfa.Application.RoleAgg
+{
+    public class RoleDomainService : IRoleDomainService
+	{
+        private readonly IRoleRepository _roleRepository;
+
+        public RoleDomainService(IRoleRepository roleRepository) => _roleRepository = roleRepository;
+
+        public bool IsTitleExist(string title) => _roleRepository.Exists(r => r.Title == title);
+        
+    }
+}
