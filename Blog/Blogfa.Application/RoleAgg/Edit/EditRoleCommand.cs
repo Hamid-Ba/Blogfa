0fd41083dc27afbf3c86eab33a1ccc4feee1a530
hamidba <balalzadehhamid79@gmail.com>
2022-04-14 20:29:06 +0430

    Complete Edit Role Command

---
+++ Blog/Blogfa.Application/RoleAgg/Edit/EditRoleCommand.cs
@@ -0,0 +1,7 @@
+﻿using Blogfa.Domain.RoleAgg.Enums;
+using Framework.Application;
+
+namespace Blogfa.Application.RoleAgg.Edit
+{
+    public record EditRoleCommand(long Id,string Title,List<Permission> Permissions) : IBaseCommand;
+}
---
+++ Blog/Blogfa.Application/RoleAgg/Edit/EditRoleCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Blogfa.Domain.RoleAgg;
+using Blogfa.Domain.RoleAgg.Repository;
+using Blogfa.Domain.RoleAgg.Services;
+using Framework.Application;
+
+namespace Blogfa.Application.RoleAgg.Edit
+{
+    internal class EditRoleCommandHandler : IBaseCommandHandler<EditRoleCommand>
+    {
+        private readonly IRoleRepository _roleRepository;
+        private readonly IRoleDomainService _roleDomainService;
+
+        public async Task<OperationResult> Handle(EditRoleCommand request, CancellationToken cancellationToken)
+        {
+            var role = await _roleRepository.GetAsTrackingAsyncBy(request.Id);
+            if (role is null) return OperationResult.NotFound();
+
+            var rolePermissions = new List<RolePermission>();
+            request.Permissions.ForEach(p => rolePermissions.Add(new RolePermission(p)));
+
+            role.Edit(request.Title, rolePermissions, _roleDomainService);
+            await _roleRepository.SaveChangesAsync();
+
+            return OperationResult.Success();
+        }
+    }
+}
