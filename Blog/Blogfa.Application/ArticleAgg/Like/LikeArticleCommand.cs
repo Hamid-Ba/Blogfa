f38454b93a3cc0032c24a30fc5c23622b75f0b64
hamidba <balalzadehhamid79@gmail.com>
2022-04-14 09:40:07 +0430

    Complete Like Article Command

---
+++ Blog/Blogfa.Application/ArticleAgg/Like/LikeArticleCommand.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using Framework.Application;
+
+namespace Blogfa.Application.ArticleAgg.Like
+{
+    public record LikeArticleCommand(long Id,long UserId) : IBaseCommand;
+}
---
+++ Blog/Blogfa.Application/ArticleAgg/Like/LikeArticleCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Blogfa.Domain.ArticleAgg.Repository;
+using Framework.Application;
+
+namespace Blogfa.Application.ArticleAgg.Like
+{
+    internal class LikeArticleCommandHandler : IBaseCommandHandler<LikeArticleCommand>
+    {
+        private readonly IArticleRepository _articleRepository;
+
+        public LikeArticleCommandHandler(IArticleRepository articleRepository) => _articleRepository = articleRepository;
+
+        public async Task<OperationResult> Handle(LikeArticleCommand request, CancellationToken cancellationToken)
+        {
+            var article = await _articleRepository.GetAsTrackingAsyncBy(request.Id);
+            if (article is null) return OperationResult.NotFound();
+
+            article.Like(request.UserId);
+            await _articleRepository.SaveChangesAsync();
+
+            return OperationResult.Success();
+        }
+    }
+}
