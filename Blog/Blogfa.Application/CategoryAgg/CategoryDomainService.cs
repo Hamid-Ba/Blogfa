f10d2f54b6821bc3576d1ddcfa10a359cad9ff60
hamidba <balalzadehhamid79@gmail.com>
2022-04-14 10:02:14 +0430

    Complete Category Domain Service Implementation

---
+++ Blog/Blogfa.Application/CategoryAgg/CategoryDomainService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Blogfa.Domain.CategoryAgg.Repository;
+using Blogfa.Domain.CategoryAgg.Services;
+
+namespace Blogfa.Application.CategoryAgg
+{
+	public class CategoryDomainService : ICategoryDomainService
+	{
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryDomainService(ICategoryRepository categoryRepository) => _categoryRepository = categoryRepository;
+
+        public bool IsSlugExist(string slug) => _categoryRepository.Exists(c => c.Slug == slug);
+        
+    }
+}
