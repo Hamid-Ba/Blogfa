a2978c2b6c40023fbb147b0fda8551ecf28379ed
hamidba <balalzadehhamid79@gmail.com>
2022-04-14 09:12:13 +0430

    Add MediatR , Request , Request Handler , ...

---
+++ Blog/Blogfa.Application/ArticleAgg/Create/CreateArticleCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Framework.Domain.ValueObjects;
+using Microsoft.AspNetCore.Http;
+
+namespace Blogfa.Application.ArticleAgg.Create
+{
+	//public record CreateArticleCommand(string Title, long UserId, long CategoryId, string Slug, IFormFile Image, SeoImage SeoImage,
+	//		string Description, SeoData SeoData) : IBaseCommand
+	
+}
+
---
+++ Framework/Application/Utils/DirectoryImages.cs
@@ -0,0 +1,9 @@
+﻿namespace Framework.Application.Utils
+{
+    public static class DirectoryImages
+    {
+        public const string Articles = "Articles";
+
+        public const string Avatars = "Avatars";
+    }
+}
