5c6380d8120f5f2242960affed395ce8cf8df398
hamidba <balalzadehhamid79@gmail.com>
2022-04-15 00:03:14 +0430

    Complete User Domain Service Implementation

---
+++ Blog/Blogfa.Application/UserAgg/UserDomainService.cs
@@ -0,0 +1,15 @@
+﻿using Blogfa.Domain.UserAgg.Repository;
+using Blogfa.Domain.UserAgg.Services;
+
+namespace Blogfa.Application.UserAgg
+{
+    public class UserDomainService : IUserDomainService
+	{
+        private readonly IUserRepository _userRepository;
+
+        public UserDomainService(IUserRepository userRepository) => _userRepository = userRepository;
+
+        public bool IsPhoneNumberExist(string phoneNumber) => _userRepository.Exists(u => u.PhoneNumber == phoneNumber);
+        
+    }
+}
