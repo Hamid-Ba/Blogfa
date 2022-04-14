787960316e60e15bc65a508968cfa8bc6fbab5d8
hamidba <balalzadehhamid79@gmail.com>
2022-04-14 09:47:01 +0430

    Complete Article Domain Service Implementation

---
+++ Blog/Blogfa.Application/ArticleAgg/ArticleDomainService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Blogfa.Domain.ArticleAgg.Repository;
+using Blogfa.Domain.ArticleAgg.Services;
+
+namespace Blogfa.Application.ArticleAgg
+{
+	public class ArticleDomainService : IArticleDomainService
+	{
+        private readonly IArticleRepository _articleRepository;
+
+        public ArticleDomainService(IArticleRepository articleRepository) => _articleRepository = articleRepository;
+
+        public bool IsSlugExist(string slug) => _articleRepository.Exists(a => a.Slug == slug);
+        
+    }
+}
