8ff56976faf42068905c99d9620be7e3133e0139
hamidba <balalzadehhamid79@gmail.com>
2022-04-16 23:39:57 +0430

    Complete User Configuration

---
+++ Blog/Blogfa.Infrastructure.EfCore/UserAgg/UserConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Blogfa.Domain.UserAgg;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blogfa.Infrastructure.EfCore.UserAgg
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User", "users");
+
+            builder.HasKey(k => k.Id);
+
+            builder.Property(p => p.FirstName).HasMaxLength(75).IsRequired();
+            builder.Property(p => p.LastName).HasMaxLength(82).IsRequired();
+            builder.Property(p => p.Password).HasMaxLength(225).IsRequired();
+            builder.Property(p => p.PhoneNumber).HasMaxLength(11).IsRequired();
+            builder.Property(p => p.Avatar).HasMaxLength(150);
+
+            builder.OwnsMany(r => r.Roles, config =>
+           {
+               config.ToTable("Role", "users");
+
+               config.HasKey(k => k.Id);
+           });
+        }
+    }
+}
---
+++ Blog/Blogfa.Infrastructure.EfCore/UserAgg/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Blogfa.Domain.UserAgg;
+using Blogfa.Domain.UserAgg.Repository;
+using Framework.Infrastructure;
+
+namespace Blogfa.Infrastructure.EfCore.UserAgg
+{
+    public class UserRepository : Repository<User> , IUserRepository
+	{
+		private readonly BlogfaContext _context;
+
+		public UserRepository(BlogfaContext context) : base(context) => _context = context;
+	}
+}
