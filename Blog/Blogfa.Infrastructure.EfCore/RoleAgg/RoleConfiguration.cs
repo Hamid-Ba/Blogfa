8575d228b9f47f003e38a55ef4829ee5c6c22c89
hamidba <balalzadehhamid79@gmail.com>
2022-04-16 23:35:24 +0430

    Complete Role Configuration

---
+++ Blog/Blogfa.Infrastructure.EfCore/RoleAgg/RoleConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Blogfa.Domain.RoleAgg;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blogfa.Infrastructure.EfCore.RoleAgg
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.ToTable("Roles", "role");
+
+            builder.HasKey(k => k.Id);
+
+            builder.Property(p => p.Title).HasMaxLength(75).IsRequired();
+
+            builder.OwnsMany(p => p.Permissions, config =>
+           {
+               config.ToTable("Permissions", "role");
+
+               config.HasKey(k => k.Id);
+           });
+        }
+    }
+}
---
+++ Blog/Blogfa.Infrastructure.EfCore/RoleAgg/RoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using Blogfa.Domain.RoleAgg;
+using Blogfa.Domain.RoleAgg.Repository;
+using Framework.Infrastructure;
+
+namespace Blogfa.Infrastructure.EfCore.RoleAgg
+{
+    public class RoleRepository : Repository<Role>, IRoleRepository
+	{
+		private readonly BlogfaContext _context;
+
+		public RoleRepository(BlogfaContext context) : base(context) => _context = context;
+	}
+}
