70ad3d97ee7ce9ef99e4a1494de1393b7630c6ae
hamidba <balalzadehhamid79@gmail.com>
2022-04-16 23:46:22 +0430

    Complete Ef Core Bootstrapper And Add Db Sets

--- Blog/Blogfa.Infrastructure.EfCore/BlogfaContext.cs
+++ Blog/Blogfa.Infrastructure.EfCore/BlogfaContext.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using Blogfa.Domain.ArticleAgg;
+﻿using Blogfa.Domain.ArticleAgg;
+using Blogfa.Domain.CategoryAgg;
+using Blogfa.Domain.CommentAgg;
+using Blogfa.Domain.RoleAgg;
+using Blogfa.Domain.UserAgg;
 using Microsoft.EntityFrameworkCore;
 
 namespace Blogfa.Infrastructure.EfCore
 {
-	public class BlogfaContext : DbContext
-	{
-		public BlogfaContext(DbContextOptions<BlogfaContext> context) : base(context) { }
+    public class BlogfaContext : DbContext
+    {
+        public BlogfaContext(DbContextOptions<BlogfaContext> context) : base(context) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -17,8 +20,16 @@ namespace Blogfa.Infrastructure.EfCore
             modelBuilder.ApplyConfigurationsFromAssembly(assembly);
 
             modelBuilder.Entity<Article>().HasQueryFilter(p => !p.IsDelete);
+            modelBuilder.Entity<Category>().HasQueryFilter(p => !p.IsDelete);
+            modelBuilder.Entity<Comment>().HasQueryFilter(p => !p.IsDelete);
+            modelBuilder.Entity<Role>().HasQueryFilter(p => !p.IsDelete);
+            modelBuilder.Entity<User>().HasQueryFilter(p => !p.IsDelete);
         }
 
         public Article Article { get; set; }
+        public Category Category { get; set; }
+        public Comment Comment { get; set; }
+        public Role Role { get; set; }
+        public User User { get; set; }
     }
 }
---
+++ Blog/Blogfa.Infrastructure.EfCore/Bootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Blogfa.Domain.ArticleAgg.Repository;
+using Blogfa.Domain.CategoryAgg.Repository;
+using Blogfa.Domain.CommentAgg.Repository;
+using Blogfa.Domain.RoleAgg.Repository;
+using Blogfa.Domain.UserAgg.Repository;
+using Blogfa.Infrastructure.EfCore.ArticleAgg;
+using Blogfa.Infrastructure.EfCore.CategoryAgg;
+using Blogfa.Infrastructure.EfCore.CommentAgg;
+using Blogfa.Infrastructure.EfCore.RoleAgg;
+using Blogfa.Infrastructure.EfCore.UserAgg;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blogfa.Infrastructure.EfCore
+{
+	public static class Bootstrapper
+	{
+		public static void EfCoreConfiure(this IServiceCollection services , string connectionString)
+        {
+			services.AddDbContext<BlogfaContext>(c => c.UseSqlServer(connectionString));
+
+			services.AddTransient<IArticleRepository, ArticleRepository>();
+			services.AddTransient<ICategoryRepository, CategoryRepository>();
+			services.AddTransient<ICommentRepository, CommentRepository>();
+			services.AddTransient<IRoleRepository, RoleRepository>();
+			services.AddTransient<IUserRepository, UserRepository>();
+		}
+	}
+}
