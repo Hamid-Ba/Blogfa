17796a31b9a76165ccc3cb94fe25a1f7ae612e03
hamidba <balalzadehhamid79@gmail.com>
2022-04-16 23:27:49 +0430

    Complete Category Configuration

---
+++ Blog/Blogfa.Infrastructure.EfCore/CategoryAgg/CategoryConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Blogfa.Domain.CategoryAgg;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blogfa.Infrastructure.EfCore.CategoryAgg
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories", "category");
+
+            builder.HasKey(k => k.Id);
+            builder.HasIndex(s => s.Slug);
+
+            builder.Property(p => p.Slug).HasMaxLength(160).IsRequired();
+            builder.Property(p => p.Title).HasMaxLength(80).IsRequired();
+
+            builder.OwnsOne(s => s.SeoData, config =>
+           {
+               config.Property(b => b.MetaDescription)
+                  .HasMaxLength(500)
+                  .HasColumnName("MetaDescription");
+
+               config.Property(b => b.MetaTitle)
+                   .HasMaxLength(500)
+                   .HasColumnName("MetaTitle");
+
+               config.Property(b => b.MetaKeyWords)
+                   .HasMaxLength(500)
+                   .HasColumnName("MetaKeyWords");
+           });
+        }
+    }
+}
---
+++ Blog/Blogfa.Infrastructure.EfCore/CategoryAgg/CategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Blogfa.Domain.CategoryAgg;
+using Blogfa.Domain.CategoryAgg.Repository;
+using Framework.Infrastructure;
+
+namespace Blogfa.Infrastructure.EfCore.CategoryAgg
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+	{
+		private readonly BlogfaContext _context;
+
+		public CategoryRepository(BlogfaContext context) : base(context) => _context = context;
+	}
+}
