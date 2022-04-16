61c760e5a008d031294538b6c0c03adda53d3297
hamidba <balalzadehhamid79@gmail.com>
2022-04-16 23:31:06 +0430

    Complete Category Configuration

---
+++ Blog/Blogfa.Infrastructure.EfCore/CommentAgg/CommentConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Blogfa.Domain.CommentAgg;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blogfa.Infrastructure.EfCore.CommentAgg
+{
+    public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.ToTable("Comments", "comment");
+
+            builder.HasKey(k => k.Id);
+
+            builder.Property(p => p.Content).HasMaxLength(1250).IsRequired();
+        }
+    }
+}
---
+++ Blog/Blogfa.Infrastructure.EfCore/CommentAgg/CommentRepository.cs
@@ -0,0 +1,13 @@
+﻿using Blogfa.Domain.CommentAgg;
+using Blogfa.Domain.CommentAgg.Repository;
+using Framework.Infrastructure;
+
+namespace Blogfa.Infrastructure.EfCore.CommentAgg
+{
+    public class CommentRepository : Repository<Comment>, ICommentRepository
+	{
+		private readonly BlogfaContext _context;
+
+		public CommentRepository(BlogfaContext context) : base(context) => _context = context;
+	}
+}
