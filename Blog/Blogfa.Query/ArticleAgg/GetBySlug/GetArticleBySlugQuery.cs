42fffab202802e3314c0dd8d8e7bff5f6e4f176b
hamidba <balalzadehhamid79@gmail.com>
2022-04-17 08:44:11 +0430

    Complete Get Article By Slug Query

---
+++ Blog/Blogfa.Query/ArticleAgg/GetBySlug/GetArticleBySlugQuery.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using Blogfa.Query.ArticleAgg.DTOs;
+using Framework.Query;
+
+namespace Blogfa.Query.ArticleAgg.GetBySlug
+{
+    public record GetArticleBySlugQuery(string Slug) : IBaseQuery<ArticleDto>;
+}
---
+++ Blog/Blogfa.Query/ArticleAgg/GetBySlug/GetArticleBySlugQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Blogfa.Infrastructure.EfCore;
+using Blogfa.Query.ArticleAgg.DTOs;
+using Framework.Query;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blogfa.Query.ArticleAgg.GetBySlug
+{
+    internal class GetArticleBySlugQueryHandler : IBaseQueryHandler<GetArticleBySlugQuery, ArticleDto>
+    {
+        private readonly BlogfaContext _context;
+
+        public GetArticleBySlugQueryHandler(BlogfaContext context) => _context = context;
+
+        public async Task<ArticleDto> Handle(GetArticleBySlugQuery request, CancellationToken cancellationToken)
+        {
+            var article = await _context.Article.FirstOrDefaultAsync(a => a.Slug == request.Slug);
+            if (article is null) return new();
+
+            return article.Map(_context);
+        }
+    }
+}
