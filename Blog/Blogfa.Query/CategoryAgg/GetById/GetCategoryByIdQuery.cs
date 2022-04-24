707ee0657c124dd9981f927f0721fee8d80cd225
hamidba <balalzadehhamid79@gmail.com>
2022-04-24 07:38:11 +0430

    Complete Get Category By Id Query

---
+++ Blog/Blogfa.Query/CategoryAgg/GetById/GetCategoryByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using Blogfa.Query.CategoryAgg.DTOs;
+using Framework.Query;
+
+namespace Blogfa.Query.CategoryAgg.GetById
+{
+    public record GetCategoryByIdQuery(long Id) : IBaseQuery<CategoryDto>;
+}
---
+++ Blog/Blogfa.Query/CategoryAgg/GetById/GetCategoryByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Blogfa.Infrastructure.EfCore;
+using Blogfa.Query.CategoryAgg.DTOs;
+using Framework.Query;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blogfa.Query.CategoryAgg.GetById
+{
+    public class GetCategoryByIdQueryHandler : IBaseQueryHandler<GetCategoryByIdQuery, CategoryDto>
+    {
+        private readonly BlogfaContext _context;
+
+        public GetCategoryByIdQueryHandler(BlogfaContext context) => _context = context;
+
+        public async Task<CategoryDto> Handle(GetCategoryByIdQuery request, CancellationToken cancellationToken)
+        {
+            var category = await _context.Category.FirstOrDefaultAsync(c => c.Id == request.Id);
+            return category.Map();
+        }
+    }
+}
