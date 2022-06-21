663ecea2bf63695da837095d0905cbf3d354e359
hamidba <balalzadehhamid79@gmail.com>
2022-06-21 11:19:31 +0430

    Complete Role Api Controller

---
+++ EndPoints/ServiceHost.Api/Controllers/RoleApiController.cs
@@ -0,0 +1,20 @@
+using Blogfa.Presentation.Facade.RoleAgg;
+using Blogfa.Query.RoleAgg.DTOs;
+using Framework.Presentation.Api;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.Api.Controllers
+{
+    public class RoleApiController : BaseApiController
+    {
+        private readonly IRoleFacade _roleFacade;
+
+        public RoleApiController(IRoleFacade roleFacade) => _roleFacade = roleFacade;
+
+        [HttpGet]
+        public async Task<ApiResult<IEnumerable<RoleDto>>> GetAll() => QueryResult(await _roleFacade.GetAll());
+
+        [HttpGet("{id}")]
+        public async Task<ApiResult<RoleDto>> GetBy(long id) => QueryResult(await _roleFacade.GetBy(id));
+    }
+}
