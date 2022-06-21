242a2780d1f790922d0f0da26b442983c8386a79
hamidba <balalzadehhamid79@gmail.com>
2022-06-21 11:12:28 +0430

    Complete Category Api Controller

---
+++ EndPoints/ServiceHost.Api/Controllers/CategoryApiController.cs
@@ -0,0 +1,23 @@
+using Blogfa.Presentation.Facade.CategoryAgg;
+using Blogfa.Query.CategoryAgg.DTOs;
+using Framework.Presentation.Api;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.Api.Controllers
+{
+    public class CategoryApiController : BaseApiController
+    {
+        private readonly ICategoryFacade _categoryFacade;
+
+        public CategoryApiController(ICategoryFacade categoryFacade) => _categoryFacade = categoryFacade;
+
+        [HttpGet]
+        public async Task<ApiResult<IEnumerable<CategoryDto>>> GetAll() => QueryResult(await _categoryFacade.GetAll());
+
+        [HttpGet("{id}")]
+        public async Task<ApiResult<CategoryDto>> GetBy(long id) => QueryResult(await _categoryFacade.GetBy(id));
+
+        [HttpGet("{slug}")]
+        public async Task<ApiResult<CategoryDto>> GetBy(string slug) => QueryResult(await _categoryFacade.GetBy(slug));
+    }
+}
