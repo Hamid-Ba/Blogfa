cb7ac912333ebfdc3fd3edc2cde508d025223f6c
hamidba <balalzadehhamid79@gmail.com>
2022-06-26 12:31:57 +0430

    Complete User APIs

--- EndPoints/ServiceHost.Api/Controllers/UserApiController.cs
+++ EndPoints/ServiceHost.Api/Controllers/UserApiController.cs
@@ -1,4 +1,7 @@
+using Blogfa.Application.UserAgg.Active;
 using Blogfa.Application.UserAgg.ChangePassword;
+using Blogfa.Application.UserAgg.Create;
+using Blogfa.Application.UserAgg.DeActive;
 using Blogfa.Application.UserAgg.Edit;
 using Blogfa.Presentation.Facade.UserAgg;
 using Blogfa.Query.UserAgg.DTOs;
@@ -21,6 +24,15 @@ namespace ServiceHost.Api.Controllers
 
         [HttpGet("{phoneNumber}")]
         public async Task<ApiResult<UserDto>> GetBy(string phoneNumber) => QueryResult(await _userFacade.GetBy(phoneNumber));
+
+        [HttpPost]
+        public async Task<ApiResult> Create([FromForm] CreateUserCommand command) => CommandResult(await _userFacade.Create(command));
+
+        [HttpPut("active")]
+        public async Task<ApiResult> Active(ActiveUserCommand command) => CommandResult(await _userFacade.Active(command));
+
+        [HttpPut("deActive")]
+        public async Task<ApiResult> DeActive(DeActiveUserCommand command) => CommandResult(await _userFacade.DeActive(command));
 
         [HttpPut]
         public async Task<ApiResult> Edit([FromForm] EditUserCommand command) => CommandResult(await _userFacade.Edit(command));
