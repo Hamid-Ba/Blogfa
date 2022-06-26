23e94472b2520365dedbc9a9bb83e356ccf9830b
hamidba <balalzadehhamid79@gmail.com>
2022-06-26 12:25:58 +0430

    Complete Category APIs

--- EndPoints/ServiceHost.Api/Controllers/CategoryApiController.cs
+++ EndPoints/ServiceHost.Api/Controllers/CategoryApiController.cs
@@ -1,3 +1,5 @@
+using Blogfa.Application.CategoryAgg.Create;
+using Blogfa.Application.CategoryAgg.Edit;
 using Blogfa.Presentation.Facade.CategoryAgg;
 using Blogfa.Query.CategoryAgg.DTOs;
 using Framework.Presentation.Api;
@@ -19,5 +21,11 @@ namespace ServiceHost.Api.Controllers
 
         [HttpGet("{slug}")]
         public async Task<ApiResult<CategoryDto>> GetBy(string slug) => QueryResult(await _categoryFacade.GetBy(slug));
+
+        [HttpPost]
+        public async Task<ApiResult> Create(CreateCategoryCommand command) => CommandResult(await _categoryFacade.Create(command));
+
+        [HttpPut]
+        public async Task<ApiResult> Edit(EditCategoryCommand command) => CommandResult(await _categoryFacade.Edit(command));
     }
 }
